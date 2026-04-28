5f6865c063bb7f7d7f34a09fad2ca9c10609888d
ComradeJustin <chinese2000ping@gmail.com>
2026-04-28 02:31:26 -0700

    Audio Fixes now we can do higher rates

--- modules/desktop/common/audio.nix
+++ modules/desktop/common/audio.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   options.modules.audio.enable = lib.mkEnableOption "PipeWire audio stack";
 
@@ -15,18 +20,27 @@
           "default.clock.min-quantum" = 512;
           # Allow sample rate switching to match source material
           # instead of resampling everything to 48kHz
-          "default.clock.allowed-rates" = [ 44100 48000 88200 96000 ];
+          "default.clock.allowed-rates" = [
+            44100
+            48000
+            88200
+            96000
+            176400
+            192000
+          ];
         };
       };
       # Cap volume at 100% (0dB) to prevent digital clipping
       wireplumber.extraConfig."90-no-overamplify" = {
-        "monitor.alsa.rules" = [{
-          matches = [{ "node.name" = "~alsa_output.*"; }];
-          actions.update-props = {
-            "channelmix.max-volume" = 1.0;
-            "channelmix.normalize" = false;
-          };
-        }];
+        "monitor.alsa.rules" = [
+          {
+            matches = [ { "node.name" = "~alsa_output.*"; } ];
+            actions.update-props = {
+              "channelmix.max-volume" = 1.0;
+              "channelmix.normalize" = false;
+            };
+          }
+        ];
       };
     };
 
